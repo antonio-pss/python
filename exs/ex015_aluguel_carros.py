b1b84fa62ce1382dd57c4a17e1f9f9e98b7bb101
Antônio Pereira da Silva Santos <antonio.p.silva.santos@gmail.com>
2024-02-23 14:02:56 -0400

    Troca de Nome

--- exs/ex015_aluguel_carros.py
+++
@@ -1,3 +0,0 @@
-dias = int(input("Quantos dias alugados? "))
-km = float(input("Quantos Km rodados? "))
-print(f"O total a pagar é {60*dias+0.15*km:.2f}")
--- exs/ex016_quebrando_numero.py
+++
@@ -1,3 +0,0 @@
-from math import trunc # Deixa apenas a parte inteira de um número.
-num = float(input("Digite um valor: "))
-print(f"O valor digitado foi {num} e a sua porção inteira é {trunc(num)}")
