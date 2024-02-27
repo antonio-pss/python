bce9f656bcdfc4015c3f11633477d16c06f9e11e
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-02-27 14:21:45 -0400

    Aula 10 sobre Operadores Condicionais If e Else

---
+++ aulas/aula010_condicional.py
@@ -0,0 +1,9 @@
+idade = int(input('Quantos anos você têm? '))
+if idade >= 18:
+    print('Você é maior de idade.')
+else:
+    print('Você é menor de idade.')
+
+carTempo = int(input('Quantos anos tem seu carro?'))
+print('Carro Novo.'if carTempo<=3 else'Carro Velho.')
+
