155c3abfc4703f7ec6885592c475a78d1f7b3a6a
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-03-01 17:32:36 -0400

    Aula sobre Cores no Terminal...

---
+++ aulas/aula011_cores_terminal.py
@@ -0,0 +1,15 @@
+# Padrão ANSI
+# \033[style; text; back m]
+# \033[0;33;44m] -
+# Estilos - 0 Nada - 1 Negrito - 4 Sublinhado - 7 Inverter
+# Texto - 30 Branco - 31 Vermelho - 32 Verde - 33 Amarelo - 34 Azul - 35 Roxo - 36 Ciano - 37 Cinza
+# Background - 40 Branco - 41 Vermelho - 42 Verde - 43 Amarelo - 44 Azul - 45 Roxo - 46 Ciano - 47 Cinza
+
+# \033[0;30;41m
+# \033[4;33;44m
+# \033[1;35;43m
+# \033[30;42m
+# \033[m
+# \033[7;30m
+
+print('\033[1;32;45mOlá, Mundo!\033[m')
