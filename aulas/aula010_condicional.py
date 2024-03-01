2a4808cb591b9733252eb1a22cfbc5f9843e07ca
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2024-03-01 17:16:47 -0400

    Atualização de estrutura condicional

--- aulas/aula010_condicional.py
+++ aulas/aula010_condicional.py
@@ -1,8 +1,12 @@
 idade = int(input('Quantos anos você têm? '))
 if idade >= 18:
     print('Você é maior de idade.')
+elif idade > 100:
+    print('Você deveria estar morto...')
+elif idade < 0:
+    print('Você ainda não nasceu...')
 else:
-    print('Você é menor de idade.')
+    print('Você é menor de idade.') # Uma estrutura condicional não precisa terminar em else, pode terminar com elif ou só o if mesmo.
 
 carTempo = int(input('Quantos anos tem seu carro?'))
 print('Carro Novo.'if carTempo<=3 else'Carro Velho.')
